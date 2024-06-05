61ce0bc27be131b2875fd8048228cef60834a95b
samx <newbiebossku23@gmail.com>
2024-06-05 10:14:56 +0700

    migarate

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::get('/cek1', function() {
+    return ('<h1>Mengecek</h1>');
+})->middleware('auth', 'verified');
 require __DIR__.'/auth.php';
